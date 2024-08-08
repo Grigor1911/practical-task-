52efbda8b2527691b47ad1933e1cc984701a6d63
Grigor1911 <grixan.1941@mail.ru>
2024-08-08 22:44:15 +0500

    Add files via upload

---
+++ practical task (переменные).py
@@ -0,0 +1,6 @@
+homeworks = 12
+hours = 1.5
+course_name = "Python"
+time_for_one_task = print(hours / homeworks)
+average_time = 0.125
+print('Курс:',course_name,',','всего задач:',homeworks,',','затрачено часов:',hours,',','среднее время выполнения',average_time,'часа.')
